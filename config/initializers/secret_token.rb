970aa4407314bccbb1cd6eb27379f57ec184b53b
Jason Lai <jason@jasonlai.net>
2010-10-10 13:49:30 +0800

    Initial project import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapDemo::Application.config.secret_token = '2adb81b36706fef786d81e190bc2e401d243ed921b59016d4afd3030976de5211d728ec1c04aadabe774a7e6461786e08bf167aa409cbee6bf2acb5f0f593ed1'
